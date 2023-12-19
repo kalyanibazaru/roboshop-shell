06aa5389e4bd864883e6642062d8aa9943a48104
Kalyanibazaru <bazaru.kalyani@gmail.com>
2023-12-19 16:59:43 +0530

    roboshop shell scripts

--- mongodb.sh
+++ mongodb.sh
@@ -43,4 +43,4 @@ sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>> $LOGFILE
 VALIDATE $? "Remote access to mongodb"
 
 systemctl restart mongod &>> $LOGFILE
-VALIDATE $? "Restarting mongodb"
+VALIDATE $? "Restarting mongodb" 
