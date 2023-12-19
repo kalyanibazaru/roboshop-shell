1353fc20eda6679138a0ecf7282c53722bd14c4e
Kalyanibazaru <bazaru.kalyani@gmail.com>
2023-12-19 17:45:28 +0530

    roboshop shell scripts

--- mongodb.sh
+++ mongodb.sh
@@ -9,46 +9,45 @@ N="\e[0m"
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
-echo "script started executing at $TIMESTAMP" &>> $LOGFILE
+echo "script stareted executing at $TIMESTAMP" &>> $LOGFILE
 
 VALIDATE(){
-if [ $1 -ne 0 ]
-then
-    echo -e "$2 .......$R FAILED $N"
-    exit 1
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILED $N"
+        exit 1
     else
-    echo -e $2 ......$G SUCCESS $N"
-fi
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
 }
 
 if [ $ID -ne 0 ]
-    then 
-    echo -e "$R Error:: please run this script with root access $N"
-    exit 1
-    else
-    echo "you are root user"
-    
-fi
+then
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1 # you can give other than 0
+else
+    echo "You are root user"
+fi # fi means reverse of if, indicating condition end
 
 cp mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
 
-VALIDATE $? "copied mongodb repo"
+VALIDATE $? "Copied MongoDB Repo"
 
 dnf install mongodb-org -y &>> $LOGFILE
 
-VALIDATE $? "Installed mongoDB"
+VALIDATE $? "Installing MongoDB"
 
 systemctl enable mongod &>> $LOGFILE
 
-VALIDATE $? "Enabling mongoDB"
+VALIDATE $? "Enabling MongoDB"
 
 systemctl start mongod &>> $LOGFILE
 
-VALIDATE $? "started mongoDB"
+VALIDATE $? "Starting MongoDB"
 
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>> $LOGFILE
 
-VALIDATE $? "Remote access to mongoDB"
+VALIDATE $? "Remote access to MongoDB"
 
 systemctl restart mongod &>> $LOGFILE
 
