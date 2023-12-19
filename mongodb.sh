b5554960a59b66a596aca5271a61cf4a04a544cc
Kalyanibazaru <bazaru.kalyani@gmail.com>
2023-12-19 16:58:16 +0530

    roboshop shell scripts

--- mongodb.sh
+++ mongodb.sh
@@ -2,15 +2,15 @@
 
 ID=$(id -u)
 
-R="\e[31m
-G="\e[32m
-Y="\e[33m
-N="\e[0m
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
-TIMESTAMP=$( DATE +%F-%H-%M-%S)
-LOGFILE="/tmp/$0-$TIMESTAMP.LOG"
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
-echo "script started executing at $TIMESTAMP" $>> $LOGFILE
+echo "script started executing at $TIMESTAMP" &>> $LOGFILE
 
 VALIDATE(){
 if [ $1 -ne 0 ]
@@ -30,17 +30,17 @@ fi
 cp mongo.repo /etc/yum.repos.d/mongo.repo $>> $LOGFILE
 VALIDATE $? "copied mongodb repo"
 
-dnf install mongodb-org -y $>> $LOGFILE
+dnf install mongodb-org -y &>> $LOGFILE
 VALIDATE $? "Installed mongodb"
 
-systemctl enable mongod $>> $LOGFILE
+systemctl enable mongod &>> $LOGFILE
 VALIDATE $? "Enabling mongodb"
 
-systemctl start mongod $>> $LOGFILE
+systemctl start mongod &>> $LOGFILE
 VALIDATE $? "started mongodb"
 
-sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf $>> $LOGFILE
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>> $LOGFILE
 VALIDATE $? "Remote access to mongodb"
 
-systemctl restart mongod $>> $LOGFILE
+systemctl restart mongod &>> $LOGFILE
 VALIDATE $? "Restarting mongodb"
