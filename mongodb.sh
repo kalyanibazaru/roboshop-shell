7f752e5bb092a0bc08b6f7d9534e8385d0707670
Kalyanibazaru <bazaru.kalyani@gmail.com>
2023-12-19 17:27:20 +0530

    roboshop shell scripts

--- mongodb.sh
+++ mongodb.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 ID=$(id -u)
-
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
@@ -14,16 +13,16 @@ echo "script started executing at $TIMESTAMP" &>> $LOGFILE
 
 VALIDATE(){
 if [ $1 -ne 0 ]
-    then
+then
     echo -e "$2 .......$R FAILED $N"
-    else
+else
     echo -e $2 ......$G SUCCESS $N"
 fi
 }
 
 if [ $ID -ne 0 ]
     then 
-    echo -e "$R Error: please run this script with root access $N"
+    echo -e "$R Error:: please run this script with root access $N"
     exit 1
     else
     echo "you are root user"
