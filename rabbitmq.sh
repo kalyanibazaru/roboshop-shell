fcebc3f871d6c7633539e3b2c3de0fd78a4ee27b
Kalyanibazaru <bazaru.kalyani@gmail.com>
2023-12-20 18:16:38 +0530

    roboshop shell script

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -10,7 +10,7 @@ MONGDB_HOST=mongodb.bkdevops.online
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
-echo "script started executing at $TIMESTAMP" &>> $LOGFILE
+echo "script stareted executing at $TIMESTAMP" &>> $LOGFILE
 
 VALIDATE(){
     if [ $1 -ne 0 ]
@@ -23,12 +23,12 @@ VALIDATE(){
 }
 
 if [ $ID -ne 0 ]
-    then
-        echo -e "$R ERROR:: please run this script with root access $N"
-        exit 1
-    else
-        echo "you are root user"
-fi
+then
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1 # you can give other than 0
+else
+    echo "You are root user"
+fi # fi means reverse of if, indicating condition end
 
 curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash &>> $LOGFILE
 
@@ -51,7 +51,9 @@ systemctl start rabbitmq-server  &>> $LOGFILE
 VALIDATE $? "Starting rabbitmq server"
 
 rabbitmqctl add_user roboshop roboshop123 &>> $LOGFILE
+
 VALIDATE $? "creating user"
 
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>> $LOGFILE
+
 VALIDATE $? "setting permission"
