19c95556f4056fa05796ec2ee731a16e0a1bdcca
Kalyanibazaru <bazaru.kalyani@gmail.com>
2023-12-20 19:29:50 +0530

    roboshop shell script

--- web.sh
+++ web.sh
@@ -48,7 +48,7 @@ cd /usr/share/nginx/html
 unzip /tmp/web.zip &>> $LOGFILE
 VALIDATE $? "Unzipping web"
 
-vim /home/centos/roboshop-shell/roboshop.service /etc/nginx/default.d/roboshop.conf &>> $LOGFILE
+cp /home/centos/roboshop-shell/roboshop.service /etc/nginx/default.d/roboshop.conf &>> $LOGFILE
 VALIDATE $? "Reverse proxy configuration"
 
 systemctl restart nginx &>> $LOGFILE
