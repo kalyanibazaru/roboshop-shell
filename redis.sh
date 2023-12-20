94bff89e64b44d7b3e048fa15fb04a4e0dacc59c
Kalyanibazaru <bazaru.kalyani@gmail.com>
2023-12-20 13:15:21 +0530

    roboshop shell script

---
+++ redis.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+MONGDB_HOST=mongodb.bkdevops.online
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "script started executing at $TIMESTAMP" &>> $LOGFILE
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILED $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
+
+if [ $ID -ne 0 ]
+    then
+        echo -e "$R ERROR:: please run this script with root access $N"
+        exit 1
+    else
+        echo "you are root user"
+fi
+
+dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>> $LOGFILE
+VALIDATE $? "Installing redis repo"
+
+dnf module enable redis:remi-6.2 -y &>> $LOGFILE
+VALIDATE $? "Enabling redis"
+
+dnf install redis -y &>> $LOGFILE
+VALIDATE $? "Installing redis"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis.conf &>> $LOGFILE
+
+systemctl enable redis &>> $LOGFILE
+VALIDATE $? "Enabling redis"
+
+systemctl start redis &>> $LOGFILE
+VALIDATE $? "Starting redis"
